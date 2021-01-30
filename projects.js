a83dfabd9c6f5a42ecfea88287800005792795f1
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-29 21:13:18 -0600

    added functions to delete unavailable projects

--- projects.js
+++ projects.js
@@ -11,6 +11,16 @@ const projects = [
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux",
   },
+  {
+    title: "Cool Project",
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg",
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed:
+      "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+  }
 ];
 
 const printToDom = (divId, textToPrint) => {
@@ -20,8 +30,9 @@ const printToDom = (divId, textToPrint) => {
 
 const createProjectCards = (arr) => {
   let domString = "";
-
-  for (let i = 0; i < arr.length; i++) {
+  
+  for (let i = 0; i < arr.length; i++) { 
+    
     domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
     <div class="img-container" style="background-image: url('${arr[i].screenshot}');"></div>
     <div class="card-body">
@@ -34,14 +45,31 @@ const createProjectCards = (arr) => {
       
     </div>
   </div>`;
+    
   }
+  
+
   printToDom('#projectsPage', domString);
 };
 
-console.log(projects);
+//console.log(projects);
+
+const availableProjects = () => {
+  for (let i = 0; i < arr.length; i++) {
+    if (projects[i].available === 'false') {
+      projects.splice(projects[i], 1);
+    }
+  }
+};
+
+
+
 
 const init = () => {
   createProjectCards(projects);
+  availableProjects();
+
+  
 };
 
 init();
